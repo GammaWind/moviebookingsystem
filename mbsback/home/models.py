f7a3d8b541f58754ca94ba65eff3a4670ecf456f
Nihar Pawade <niharpawade@outlook.com>
2021-03-02 03:12:32 +0530

    added apps

---
+++ mbsback/home/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models.query import ModelIterable
+from django.core.validators import RegexValidator
+
+# Create your models here.
+class User(models.Manager):
+    email       =   models.EmailField(max_length=255,unique=True,primary_key=True)
+    phone_regex =   RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number=  models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    active      =   models.BooleanField(default=True)
+    staff       =   models.BooleanField(default=False)
+    admin       =   models.BooleanField(default=False)
+    timestamp   =   models.DateTimeField(auto_now_add=True)
+    password    =   models.CharField(max_length=50,default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS= []
+
+
+# class UserCred(models.Manager):
+
+#     email      =  models.OneToOneField(User,on_delete=models.CASCADE,unique=True)
+#     password   = models.CharField()
--- mbsback/mbsback/settings.py
+++ mbsback/mbsback/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     #added by me
     'corsheaders',
     'rest_framework',
+    'home',
 ]
 
 MIDDLEWARE = [
