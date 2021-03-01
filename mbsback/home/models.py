5b6f42613fe582fcf528a9be46ac56c8c4760788
Nihar Pawade <niharpawade@outlook.com>
2021-03-02 04:18:00 +0530

    usermodel

--- mbsback/home/models.py
+++ mbsback/home/models.py
@@ -3,7 +3,7 @@ from django.db.models.query import ModelIterable
 from django.core.validators import RegexValidator
 
 # Create your models here.
-class User(models.Manager):
+class User(models.Model):
     email       =   models.EmailField(max_length=255,unique=True,primary_key=True)
     phone_regex =   RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number=  models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
