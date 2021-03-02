4a1ad9b0e83abfe6794dc0c2b263aa9a691d05f7
nihar <niharpawade@outlook.com>
2021-03-02 17:41:55 +0530

    usercreation

--- mbsback/home/serializers.py
+++ mbsback/home/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import get_user_model
 
-class TodoSerializer(serializers.ModelSerializer):
+from django.contrib.auth.hashers import make_password
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text='Leave empty if no change needed',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
     class Meta:
-        model = User
-        fields = ('email', 'first_name', 'last_name', 'phone_number')
+        model = User = get_user_model()
+        fields = ('email', 'first_name', 'last_name', 'phone_number','password')
+
+    
+    def create(self, validated_data):
+        password = make_password(validated_data.pop('password'))
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- mbsback/home/views.py
+++ mbsback/home/views.py
@@ -1,12 +1,30 @@
+from re import DEBUG
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import TodoSerializer
+from .serializers import UserSerializer
 from .models import User
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
 class UserView(viewsets.ModelViewSet):
-    serializer_class = TodoSerializer
+    serializer_class = UserSerializer
     queryset = User.objects.all()
-
-
+    # DEBUG
+    # @action(detail=True, methods=['post'])
+    # def create_auth(request):
+    #     serialized = UserSerializer(data=request.DATA)
+    #     if serialized.is_valid():
+    #         User.objects.create_user(
+    #             serialized.init_data['email'],
+    #             serialized.init_data['fist_name'],
+    #             serialized.init_data['last_name'],
+    #             serialized.init_data['phonr_number'],
+    #             make_password(serialized.init_data['password'])
+    #         )
+    #         return Response(serialized.data, status=status.HTTP_201_CREATED)
+    #     else:
+    #         return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
